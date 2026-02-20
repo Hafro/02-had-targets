e1af56792e1a7047a64445878a6843135e28a9fb
Jamie Lentin <jm@lentin.co.uk>
2026-02-20 14:07:34 +0000

    techreport: Don't have mutilingual graphs

--- script_techreport.R
+++ script_techreport.R
@@ -30,45 +30,25 @@ list(
     format = pax::pax_tar_format_parquet()
   ),
 
-  tar_map(
-    tar_target(
-      fig_catch_dist_plot,
-      ft_catch_dist_plot(pax_db, year_start = 1989, lang = lang) |>
-        ggplot2::ggsave(
-          filename = paste0("figs/catch_dist_plot_", lang, ".jpg"),
-          width = 10,
-          height = 15,
-          units = 'in',
-          dpi = 300
-        ),
-      format = "file"
-    ),
-    tar_target(
-      fig_catch_dist_selectyrs_plot,
-      ft_catch_dist_selectyrs_plot(
-        pax_db,
-        years = c(2001, 2005, 2010, 2015, year_end - 1),
-        lang = lang
-      ) |>
-        ggplot2::ggsave(
-          filename = paste0("figs/catch_dist_selectyrs_plot_", lang, ".jpg"),
-          width = 10,
-          height = 5,
-          units = 'in',
-          dpi = 300
-        ),
-      format = "file"
-    ),
+  tar_target(
+    fig_catch_dist_plot,
+    ft_catch_dist_plot(pax_db, year_start = 1989),
+    format = "rds"
+  ),
 
-    ## i.e. run targets for lang = en & lang = is
-    values = list(lang = c("en", "is"))
+  tar_target(
+    fig_catch_dist_selectyrs_plot,
+    ft_catch_dist_selectyrs_plot(
+      pax_db,
+      years = c(2001, 2005, 2010, 2015, year_end - 1)
+    ),
+    format = "rds"
   ),
 
   ## Technical reports
-  # TODO: tar_map can't control path, so duplicating targets manually. Any other options?
   tar_quarto(
     techreport_en,
-    path = paste0("techreport_en.qmd"),
+    path = "techreport_en.qmd",
     execute_params = list(
       year_end = year_end
     ),
@@ -76,7 +56,7 @@ list(
   ),
   tar_quarto(
     techreport_is,
-    path = paste0("techreport_is.qmd"),
+    path = "techreport_is.qmd",
     execute_params = list(
       year_end = year_end
     ),
